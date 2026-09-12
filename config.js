37c75653a11bfedd0a69e1266fe08d129f9768de
Darius <dariusistoc55@gmail.com>
2026-09-12 16:41:08 +0200

    Change SUPABASE_KEY in config.js
    Updated SUPABASE_KEY with a new value.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const GAME_CONFIG = {
   SUPABASE_URL: "https://bqewnmhdghaxxvtmpebp.supabase.co",
-  SUPABASE_KEY: "METS_TA_CLE_ANON_ICI",
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxZXdubWhkZ2hheHh2dG1wZWJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMTYyMDUsImV4cCI6MjEwNDc5MjIwNX0.nj5jkQVSqe1hcPxIMpWbj_FjlsI5slWqcyLgspS_vl8",
 
   phases: {
     1: {
